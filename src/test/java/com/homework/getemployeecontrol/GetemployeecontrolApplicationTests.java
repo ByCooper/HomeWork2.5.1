ae45dc606fbcb4acd35d28d1aafd1e139ff7cfc7
ByCooper <kup.official@yahoo.com>
2023-06-09 19:19:53 +0300

    Скелет

---
+++ src/test/java/com/homework/getemployeecontrol/GetemployeecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.getemployeecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetemployeecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
